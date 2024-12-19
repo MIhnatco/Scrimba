49017fd27f51f4798540852c37c18984fdbf4c48
Ihliana <Ihnatco@protonmail.com>
2024-12-19 13:33:46 +0100

    initial commit meme_generator

---
+++ meme_generator/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+
+/**
+ * A React a
+ * @returns {JSX.Element}
+ */
+
+function App() {
+  
+
+  return (
+    <main>
+     
+       
+    </main>
+  );
+}
+
+export default App;
