0853f7aae711a18de1259fd37abd5151a92c1830
Ihliana <Ihnatco@protonmail.com>
2024-12-22 09:20:28 +0100

    useState hook and controlled components

--- meme_generator/src/components/Main.jsx
+++ meme_generator/src/components/Main.jsx
@@ -1,32 +1,55 @@
 import React from "react";
 
 function Main() {
+  const [meme, setMeme] = React.useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  function handleChange(event) {
+    setMeme((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
   return (
     <main>
       <div className="form">
-        <label htmlFor="topText">Top Text </label>
-        <input
-          type="text"
-          placeholder="One does not simply"
-          name="topText"
-          id="topText"
-        />
+        <div>
+          <label htmlFor="topText">Top Text </label>
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            id="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
+        </div>
 
-        <label htmlFor="bottomText">Bottom Text </label>
-        <input
-          type="text"
-          placeholder="Walk into Mordor"
-          name="bottomText"
-          id="bottomText"
-        />
+        <div>
+          <label htmlFor="bottomText">Bottom Text </label>
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            id="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
+        </div>
 
         <button>Get a new meme image</button>
       </div>
 
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" alt="random image" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imageUrl} alt="random image" />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
