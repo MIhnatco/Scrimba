eca181b1638b4817c281004ece80df413ad2cbf4
Ihliana <Ihnatco@protonmail.com>
2024-12-30 12:02:11 +0100

    JS Doc

--- meme_generator/src/components/Main.jsx
+++ meme_generator/src/components/Main.jsx
@@ -1,12 +1,37 @@
 import React, { useEffect } from "react";
 
+
+
+/**
+ * Main component for the Meme Generator app
+ * Allows users to create memes by customizing text and generating random meme images.
+ * 
+ * @component
+ * @returns {JSX.Element}
+ */
+
+
 function Main() {
+  /**
+   * State for storing the current meme data
+   * 
+   * @typedef {Object} meme
+   * @property {string} topText - The text displayed at the top of the meme
+   * @property {string} bottomText - The text displayed at the bottom of the meme
+   * @property {string} imageUrl - The URL of the meme image
+   */
+
   const [meme, setMeme] = React.useState({
     topText: "One does not simply",
     bottomText: "Walk into Mordor",
     imageUrl: "http://i.imgflip.com/1bij.jpg",
   });
 
+
+  /**
+   * Handles changes to the text inputs and updates the meme state
+   * @param {*React.ChangeEvent<HTMLInputElement>} event - The input change event
+   */
   function handleChange(event) {
     setMeme((prevState) => {
       return {
@@ -16,14 +41,29 @@ function Main() {
     });
   }
 
+
+  /**
+   * State for storing all available memes fetched from the API
+   * @type {Array<Object>}
+   */
   const [allMemes, setAllMemes] = React.useState([]);
 
+
+  /**
+   * Fetches meme templates from the Imgflip API on component mount
+   * Updates the 'allMemes' state with the fetched data
+   */
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
       .then((data) => setAllMemes(data.data.memes));
   }, []);
 
+
+  /**
+   * Generates a new random meme image by selecting a random URL
+   * from the 'allMemes' state and updates the meme state.
+   */
   function getMemeImage() {
     const randomNumber = Math.floor(Math.random() * allMemes.length);
     const newMemeUrl = allMemes[randomNumber].url;
