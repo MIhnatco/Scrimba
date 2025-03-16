08289d4c07c90cb8a164f8f07239a9c62da5b72d
Ihliana <Ihnatco@protonmail.com>
2025-03-16 16:26:35 +0100

    Updated 0.3.0 version - added Footer component

--- van_life/src/App.jsx
+++ van_life/src/App.jsx
@@ -5,11 +5,13 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans/Vans";
 
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <article className="max-w-3/5 mx-auto">
       <header className="h-28 flex items-center py-2.5 px-8">
-        <Link className="mr-auto text-2xl" to="/">
+        <Link className="mr-auto text-2xl font-bold" to="/">
           #VANLIFE
         </Link>
         <nav className="flex gap-4">
@@ -27,6 +29,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
       </Routes>
+
+      <Footer />
     </article>
   );
 }
---
+++ van_life/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react" 
+
+function Footer(){
+
+    return(
+        <footer className="bg-[#161616] text-[#aaa] h-18 flex shrink-0 justify-center items-center my-12  font-medium">&#169; 2023 #VANLIFE</footer>
+    )
+}
+
+export default Footer
