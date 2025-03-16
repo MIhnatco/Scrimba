ae1dbad0bd85f4d3f8a517a45918e3cfbb381253
Ihliana <Ihnatco@protonmail.com>
2025-03-16 16:19:35 +0100

    Updated version 0.2.0 - About page

--- van_life/src/App.jsx
+++ van_life/src/App.jsx
@@ -3,20 +3,29 @@ import "./App.css";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans/Vans";
 
 function App() {
   return (
     <article className="max-w-3/5 mx-auto">
-      <header className="h-24 flex items-center ">
-        <Link to="/">#VANLIFE</Link>
-        <nav>
-          <Link to="/about">About</Link>
+      <header className="h-28 flex items-center py-2.5 px-8">
+        <Link className="mr-auto text-2xl" to="/">
+          #VANLIFE
+        </Link>
+        <nav className="flex gap-4">
+          <Link className="text-xl" to="/about">
+            About
+          </Link>
+          <Link className="text-xl" to="/vans">
+            Vans
+          </Link>
         </nav>
       </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </article>
   );
--- van_life/src/pages/About.jsx
+++ van_life/src/pages/About.jsx
@@ -1,8 +1,41 @@
-function About(){
-    return(
-        <p>About component</p>
-    )
-}
+import { Link } from "react-router-dom";
+import aboutHeroImg from "../assets/images/about-hero.png";
 
+function About() {
+  return (
+    <article className="my-8">
+      <img
+        src={aboutHeroImg}
+        alt="man sitting on a van in the night"
+        className="max-w-full"
+      />
+
+      <section className="text-[#161616] mb-3.5 px-6">
+        <h1 className="font-extrabold my-12 text-4xl">
+          Don't squeeze in a sedan when you could relax in a van.
+        </h1>
+
+        <p className="text-xl my-8">
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch cost extra 😉)
+        </p>
+        <p className="text-xl my-8">
+          Our tema is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+      </section>
+      <section className="bg-[#ffcc8d] text-[#161616] pb-8 rounded-md px-8 mx-6">
+        <h2 className="pt-9.25 pb-9.25 text-xl font-extrabold">
+          Your destination is waiting. <br /> Your van is ready.
+        </h2>
+
+        <Link className="bg-[#161616] p-2 text-white rounded-xl" to="/vans">
+          Explore our vans.
+        </Link>
+      </section>
+    </article>
+  );
+}
 
 export default About;
